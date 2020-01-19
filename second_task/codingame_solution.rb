00a7f0e45c01b34340012d3f17b069efbe00c535
Nikolay <Xrazik@yandex.ru>
2020-01-19 17:06:57 +0300

    chore: split numbers input, make an array of numbers, sort them by ascending

---
+++ second_task/codingame_solution.rb
@@ -0,0 +1,11 @@
+# https://www.codingame.com/training/hard/simplify-selection-ranges
+
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+numbers = gets.chomp.tr('[]', '').split(',').map(&:to_i).sort
+
+# Write an action using puts
+# To debug: STDERR.puts "Debug messages..."
+
