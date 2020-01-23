d92914401f2a3b376300ce318a1ebefaed2f62ff
Nikolay <Xrazik@yandex.ru>
2020-01-23 19:16:48 +0300

    chore: remove combinations that have candies count more than count which Terry can put in the mouth

--- third_task/codingame_solution.rb
+++ third_task/codingame_solution.rb
@@ -9,6 +9,12 @@ n, k = gets.split(" ").collect {|x| x.to_i}
 result      = []
 sum_members = Array(1..n)
 
+def remove_too_many_candies(combos, max)
+  combos.select do |combo|
+    combo.select {|candies| candies > max }.empty?
+  end
+end
+
 def remove_incorrect_sum(combos, sum)
   combos.select { |combo| combo.sum == sum  }
 end
@@ -17,5 +23,6 @@ end
   combos = sum_members.repeated_permutation(index).to_a
 
   combos = remove_incorrect_sum(combos, n)
+  combos = remove_too_many_candies(combos, k)
 
 end
