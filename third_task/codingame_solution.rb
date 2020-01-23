dfee988e4ff3cf409aff89f8d56beadfd1d8869e
Nikolay <Xrazik@yandex.ru>
2020-01-23 19:32:53 +0300

    feat: added comments, task finished

--- third_task/codingame_solution.rb
+++ third_task/codingame_solution.rb
@@ -1,6 +1,10 @@
+# https://www.codingame.com/training/hard/simplify-selection-ranges
+# All the tests has successfully passed
+
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
+# Преобразовать два вводимых числа-строки в массив из чисел
 n, k = gets.split(" ").collect {|x| x.to_i}
 
 # Write an action using puts
@@ -9,17 +13,25 @@ n, k = gets.split(" ").collect {|x| x.to_i}
 result      = []
 sum_members = Array(1..n)
 
+# Удаление из возможной комбинации количества конфет, которое не влезает Терри в рот за один раз.
 def remove_too_many_candies(combos, max)
   combos.select do |combo|
-    combo.select {|candies| candies > max }.empty?
+    combo.select { |candies| candies > max }.empty?
   end
 end
 
+# Удаление комбинпций, которые не суммируются в общее число имеющихся у Терри конфет.
 def remove_incorrect_sum(combos, sum)
-  combos.select { |combo| combo.sum == sum  }
+  combos.select { |combo| combo.sum == sum }
 end
 
+# Цикл, длинной со всем количеством конфет от 1 до имеющегося количества конфет
+
 1.upto(n) do |index|
+  # Метод repeated_combinations первым аргументом принимает длинну комбинаций
+  # Максимальная длина комбинаций будет равна имеющемуся количеству конфет у Терри,
+  # так как (к примеру) при количестве конфет равному 5, самая длинная комбинация будет
+  # [1, 1, 1, 1, 1] и не длиннее.
   combos = sum_members.repeated_permutation(index).to_a
 
   combos = remove_incorrect_sum(combos, n)
@@ -28,7 +40,11 @@ end
   result << combos unless combos.empty?
 end
 
+# Раскрыть один уровень массивов, так как result хранит [[[1, 2], [2, 1]], [[1, 1, 1]]]
+# и имеет 3 уровня массивов
+# Отсортировать все комбинации по возрастанию
 result.flatten(1).sort.each do |combo|
+  # Преобразовать комбинацию вида [1, 1, 1] в строку '1 1 1'
   combo = combo.join(' ')
   puts combo
 end
